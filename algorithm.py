fea7c84e8a42214c1874ebc73af197f1e7a72ae8
alikatgullon <aliciakgullon@gmail.com>
2024-09-23 13:36:47 -0700

    Add comment for future alterations

--- algorithm.py
+++ algorithm.py
@@ -10,7 +10,8 @@ with open('activemembersFA24.json', 'r') as file:
     users = json.load(file)
 
 # Initialize a set to track user pairs that have been together
-past_groupings = set()
+past_groupings = {frozenset({'Aarushi Shah', 'Victor Zhou'}), frozenset({'Andrei Cojocaru', 'Jaewon Lee'}), frozenset({'Raghav Punnam', 'Esme Can'}), frozenset({'Iyyah Zareef-Mustafa', 'John Glen Siy'}), frozenset({'Liz Weaver', 'Phillip Chen'}), frozenset({'Emma Zhong', 'Euhan Kim'}), frozenset({'Philip Ye', 'Phillip Chen'}), frozenset({'Timothy Park', 'Euhan Kim'}), frozenset({'Minjune Kim', 'Hailey Pham'}), frozenset({'Max DeFanti', 'Grace Juan'}), frozenset({'Iyyah Zareef-Mustafa', 'Rachel Sang'}), frozenset({'Vivek Putta', 'Justin Xu'}), frozenset({'Rohan Gulati', 'Samhita Ghosh'}), frozenset({'Yunze Du', 'Kate Trevor'}), frozenset({'Vivek Putta', 'Ting-Yong Liu'}), frozenset({'Dara Lin', 'Natalie Wei'}), frozenset({'Colleen Pangilinan', 'Victor Zhou'}), frozenset({'Alana Yang', 'Jaewon Lee'}), frozenset({'Rachel Sang', 'John Glen Siy'}), frozenset({'Raghav Punnam', 'Maithreya Gowda'}), frozenset({'Allan Chen', 'Angie Baik'}), frozenset({'Stephane Finot', 'Alyssa Smith'}), frozenset({'April Zhang', 'Neil Kamath'}), frozenset({'Henrique Ribeiro Rodrigues', 'Danica Tang'}), frozenset({'Max DeFanti', 'Amber Gupta'}), frozenset({'Henrique Ribeiro Rodrigues', 'Miller Liu'}), frozenset({'Xina Wang', 'Alicia Gullon'}), frozenset({'Kelly Yeh', 'Alvin Lin'}), frozenset({'Maithreya Gowda', 'Esme Can'}), frozenset({'Sahir Tandon', 'Heidy Hernandez'}), frozenset({'Rohan Gulati', 'Shing Gao'}), frozenset({'Amber Gupta', 'Grace Juan'}), frozenset({'John Bragado', 'Hailey Pham'}), frozenset({'Sajiv Kumar', 'Kate Trevor'}), frozenset({'Alyssa Smith', 'Navi Emiliano'}), frozenset({'Angela Lee', 'Neil Kamath'}), frozenset({'Liz Weaver', 'Philip Ye'}), frozenset({'Xina Wang', 'Anna Chung'}), frozenset({'Christina Yue', 'Alvin Lin'}), frozenset({'Ava Shah', 'Natalie Wei'}), frozenset({'Dhruv Ganesh', 'Ria Jain'}), frozenset({'Danica Tang', 'Miller Liu'}), frozenset({'Sahir Tandon', 'Danny Tran'}), frozenset({'Angie Baik', 'Alan Wei'}), frozenset({'Timothy Park', 'Emma Zhong'}), frozenset({'Kevin Hu', 'Andrew Choy'}), frozenset({'Christina Yue', 'Kelly Yeh'}), frozenset({'Heidy Hernandez', 'Danny Tran'}), frozenset({'Alicia Gullon', 'Anna Chung'}), frozenset({'Alena Chao', 'Aditi Mundra'}), frozenset({'April Zhang', 'Angela Lee'}), frozenset({'Alex Chen', 'Ria Jain'}), frozenset({'Stephane Finot', 
+'Navi Emiliano'}), frozenset({'Victoria Nguyen', 'Andrew Choy'}), frozenset({'Kevin Hu', 'Victoria Nguyen'}), frozenset({'Allan Chen', 'Alan Wei'}), frozenset({'Justin Xu', 'Ting-Yong Liu'}), frozenset({'Sajiv Kumar', 'Yunze Du'}), frozenset({'Alena Chao', 'Sarah Kiefer'}), frozenset({'Colleen Pangilinan', 'Aarushi Shah'}), frozenset({'Andrei Cojocaru', 'Alana Yang'}), frozenset({'Samhita Ghosh', 'Shing Gao'}), frozenset({'Alex Chen', 'Dhruv Ganesh'}), frozenset({'Ava Shah', 'Dara Lin'}), frozenset({'John Bragado', 'Minjune Kim'}), frozenset({'Aditi Mundra', 'Sarah Kiefer'})}
 
 def have_been_together(user1, user2):
     '''Check if two users have been in the same group before.'''
@@ -96,5 +97,7 @@ for i, group in enumerate(diverse_groups):
 print("Ungrouped Members:", [user['Name'] for user in ungrouped_members])
 
 print("Past groupings:", past_groupings)
+
+print("past groupings length: ", len(past_groupings))
 
 
